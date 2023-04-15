79e38f60d59068c67131283ab16fc5258d096006
Pasha <pashamedvedev03@gmail.com>
2023-04-15 08:02:05 +0000

    simple echo bot

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello world!")
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="12345678:AaBbCcDdEeFfGgHh")
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
