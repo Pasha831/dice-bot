3f246539ee05585ecdc81e22fde0605a43a136aa
Pasha <pashamedvedev03@gmail.com>
2023-04-15 08:30:03 +0000

    add buttons, delete useless shit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import os
 
+from datetime import datetime
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from dotenv import load_dotenv
@@ -19,15 +20,17 @@ dp = Dispatcher()
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
-
-@dp.message(Command("dice"))
-async def cmd_dice(message: types.Message):
-    await message.answer_dice(emoji="🎲")
-
-@dp.message(Command("basket"))
-async def cmd_dice(message: types.Message):
-    await message.answer_dice(emoji="🏀")
+    kb = [
+        [
+            types.KeyboardButton(text="🏀"),
+            types.KeyboardButton(text="🎲")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True
+    )
+    await message.answer("Hello!", reply_markup=keyboard)
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
